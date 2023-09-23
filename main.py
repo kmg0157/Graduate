091742dbb333f0e6931f8c36d3ab5914f3c4f97e
kmg0157 <kmg0157@naver.com>
2023-09-23 18:10:45 +0900

    용희형 감사해용! 주석 처리 및 코드 정리 완료

--- main.py
+++ main.py
@@ -2,26 +2,27 @@ from TCP_server import Server
 from json_file import write_json
 
 def main():
-    TCP_server = Server()    #소켓 객체 생성
-    write_data=write_json()       #json 파일 작성 객체 생성
+    TCP_server = Server()   #서버 소켓 객체 생성
+    write_data=write_json() #json 파일 객체 생성
 
-    write_data.open_file()    #json 파일 오픈
-    TCP_server.start_server()
-    TCP_server.accept_socket()
+    write_data.open_file()    #json 파일 열기
+    TCP_server.start_server()   #서버 소켓 생성
+    TCP_server.accept_socket()  #클라이언트와 연결 수립
     
     try:
         while True:        
-            data = TCP_server.client_socket.recv(1024)
-            decoded_data = data.decode('utf-8')
+            data = TCP_server.client_socket.recv(1024)  #클라이언트로부터 최대 1024bytes 데이터 받기
+            decoded_data = data.decode('utf-8') #데이터 디코딩
             print(decoded_data)
-            if decoded_data=="CLOSE":
+            if decoded_data=="CLOSE":   #클라이언트 종료 문자열 수신 시
                 break
             else:
-                write_data.save_data(decoded_data)
+                write_data.save_data(decoded_data)  #전달받은 데이터 json 파일에 작성
         
     finally: 
-        write_data.close_file()
-        TCP_server.client_socket.close()
+        write_data.close_file() #json 파일 닫기
+        TCP_server.client_socket.close()    #클라이언트 소켓 종료
+        TCP_server.server_socket.close()    #서버 소켓 종료
 
 if __name__ == "__main__":
     main()
