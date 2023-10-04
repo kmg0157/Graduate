08d9b377235274f1ee15c959ff67c92b9fcbba50
kmg0157 <kmg0157@naver.com>
2023-10-04 22:32:40 +0900

    add DB system

---
+++ gps_db.py
@@ -0,0 +1,29 @@
+import pymysql
+import json
+
+class Database:
+    def __init__(self):
+        self.conn=None
+        self.cur=None
+
+    # 생성해둔 DB 접속
+    def connect_db(self):
+        self.conn=pymysql.connect(host="localhost",port=3306,user="root",password="0157",db="gpsdb",charset="utf8")   #DB 연결
+        self.cur=self.conn.cursor() #커서 생성
+
+    # 데이터 삽입 쿼리 실행
+    def save_data(self,data):
+        parshed_data=json.loads(data)  #데이터 파싱
+        #데이터 저장
+        insert_query = "INSERT INTO data (Sequence, Timestamp, Latitude, Longitude) VALUES (%s, %s, %s, %s)"
+        self.cur.execute(insert_query, (parshed_data["Sequence"], parshed_data["Timestamp"], parshed_data["Latitude"], parshed_data["Longitude"]))
+        
+
+    # DB 연결 종료
+    def close_db(self):
+        self.conn.commit()  #테이블 변경사항 저장
+        self.cur.close()    #커서 종료
+        self.conn.close()   #DB 접속 종료
+
+
+
--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 from TCP_server import Server
 from json_file import write_json
+from gps_db import Database
 
 def main():
     TCP_server = Server()   #서버 소켓 객체 생성
     write_data=write_json() #json 파일 객체 생성
+    save_db=Database()  #DB 저장 객체 생성
 
     write_data.open_file()    #json 파일 열기
     TCP_server.start_server()   #서버 소켓 생성
     TCP_server.accept_socket()  #클라이언트와 연결 수립
-    
+    save_db.connect_db()    #DB 연결
     try:
         while True:        
             data = TCP_server.client_socket.recv(1024)  #클라이언트로부터 최대 1024bytes 데이터 받기
             decoded_data = data.decode('utf-8') #데이터 디코딩
-            print(decoded_data)
+            print(decoded_data) # 전달 받은 데이터 출력
             if decoded_data=="CLOSE":   #클라이언트 종료 문자열 수신 시
                 break
             else:
                 write_data.save_data(decoded_data)  #전달받은 데이터 json 파일에 작성
-        
+                save_db.save_data(decoded_data) #DB에 데이터 저장
+
     finally: 
         write_data.close_file() #json 파일 닫기
+        save_db.close_db()  #DB 연결 종료
         TCP_server.client_socket.close()    #클라이언트 소켓 종료
         TCP_server.server_socket.close()    #서버 소켓 종료
 
