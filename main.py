32174090570d103042a91c2aa7a4d2c67d684b1d
kmg0157 <kmg0157@naver.com>
2023-10-29 20:47:51 +0900

    showlocation

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from TCP_server import Server
 from json_file import write_json
 from gps_db import Database
+from show_gps import Map
 
 def main():
     TCP_server = Server()   #서버 소켓 객체 생성
     write_data=write_json() #json 파일 객체 생성
     save_db=Database()  #DB 저장 객체 생성
+    m=Map()
 
     write_data.open_file()    #json 파일 열기
     TCP_server.start_server()   #서버 소켓 생성
     TCP_server.accept_socket()  #클라이언트와 연결 수립
     save_db.connect_db()    #DB 연결
+
+    mark=0
+
     try:
         while True:        
             data = TCP_server.client_socket.recv(1024)  #클라이언트로부터 최대 1024bytes 데이터 받기
@@ -21,6 +26,10 @@ def main():
             else:
                 write_data.save_data(decoded_data)  #전달받은 데이터 json 파일에 작성
                 save_db.save_data(decoded_data) #DB에 데이터 저장
+                if mark<1:
+                    m.read_data(decoded_data)
+                    m.show_map()
+                    mark+=1
 
     finally: 
         write_data.close_file() #json 파일 닫기
---
+++ show_gps.py
@@ -0,0 +1,22 @@
+import folium
+import json
+import webbrowser
+
+class Map:
+    def __init__(self):
+        self.locate=None        
+        self.latitude=None
+        self.longitude=None
+        self.save_html_file='crash.html'
+
+    def read_data(self,data):
+        parsed_data=json.loads(data)
+        self.latitude=parsed_data['Latitude']
+        self.longitude=parsed_data['Longitude']
+
+    def show_map(self):       
+        self.locate=folium.Map(location=[self.latitude,self.longitude],zoom_start=18)
+        folium.Marker([self.latitude,self.longitude], popup='사고 발생 위치!').add_to(self.locate)        
+        
+        self.locate.save(self.save_html_file)
+        webbrowser.open(self.save_html_file)
